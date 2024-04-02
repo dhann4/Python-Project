1f1f3aaa5a28f094dd388bcadfc7372223b54082
Fu_Ha <78632939+dhann4@users.noreply.github.com>
2024-04-02 09:44:45 +0800

    Update jam.py

--- jam.py
+++ jam.py
@@ -12,16 +12,16 @@ print("=" * 60 + "\n" + "=" * 24 + "-|" + "Tanggal dan Waktu" + "|-" + "=" * 22)
 print("=" * 16 + "-|" + jamLokal + "|-" + "=" * 16)
 
 if 0 <= jam <= 6:
-    print("=" * 21 + "-<|Selamat Subuh|>-" + "=" * 20)
+    print("=" * 21 + "-<|Selamat subuh|>-" + "=" * 20)
 
 elif 6 <= jam <= 12:
-    print("=" * 21 + "-<|Selamat Pagi|>-" + "=" * 21)
+    print("=" * 21 + "-<|Selamat pagi|>-" + "=" * 21)
 
 elif 12 <= jam <= 14:
-    print("=" * 21 + "-<|Selamat Siang|>-" + "=" * 20)
+    print("=" * 21 + "-<|Selamat siang|>-" + "=" * 20)
 
 elif 14 <= jam <= 18:
-    print("=" * 22 + "-|Selamat Sore|-" + "=" * 22)
+    print("=" * 22 + "-|Selamat sore|-" + "=" * 22)
 
 elif 18 <= jam <= 24:
-    print("=" * 21 + "-<|Selamat Malam|>-" + "=" * 20)
+    print("=" * 21 + "-<|Selamat malam|>-" + "=" * 20)
